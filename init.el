e294b04d25b4d121260e597173c1a87902969838
Paul Magwene <paul.magwene@duke.edu>
2014-11-06 00:57:31 -0500

    Minor fixes to init.el to suppress symbolic link warnings https --> http for Melpa

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+; suppress annoying messages about symbolic links
+; to Git tracked files
+(setq vc-follow-symlinks t)
+(setq vc-suppress-confirm t)
+
+; Get to the actual job of loading the config
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file 
